7f211955d5d2665e52f7a0e416f3c5eb94c76c6a
ACEHiGH1 <geri.dhima@yahoo.com>
2025-05-16 18:25:31 -0400

    EKS Cluster terraform Creation

---
+++ eks-terraform/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket         = "devops-tetris"
+    region         = "us-west-2"
+    key            = "devops-demo/eks/terraform.tfstate"
+    dynamodb_table = "dynamodb-terraform-devops-locking"
+    encrypt        = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
